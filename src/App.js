82c4789a717138e95514b1ee7477f826021f5788
owlsspell <arhipowael@gmail.com>
2022-06-20 16:25:00 +0300

    change loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import { Provider } from "react-redux";
 import "./App.css";
 import BeersContainer from "./components/BeerContainer";
-import store from "./redux/redux-store"
+import store from "./redux/redux-store";
+import { useEffect, useState } from "react";
+import spinner from "./assets/Spinner-1s-200px.svg";
+
 
 function App() {
-  return  <Provider store={store}><BeersContainer /></Provider>;
+  const [loading, setLoading] = useState(true);
+
+  const onPageLoad = () => {
+    if (document.readyState == "complete") {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("load", onPageLoad);
+    return () => window.removeEventListener("load", onPageLoad);
+  }, []);
+  return (
+    <Provider store={store}>
+      {loading ? (
+        <div className="spinner_container">
+          <img src={spinner} alt="" />
+        </div>
+      ) : (
+        <BeersContainer />
+      )}
+    </Provider>
+  );
 }
 
 export default App;
